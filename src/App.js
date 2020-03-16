268c0628f4b5d6e9f25c198a8013279d5f435cdd
YasserYka <yasseryka@gmail.com>
2020-03-16 22:41:56 +0300

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Working?</h1>
+        <Navbar />
       </div>
     );
   }
