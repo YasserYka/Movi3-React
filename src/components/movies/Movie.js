0eb2761c8d8e8eb4fada58ab895192d51b08ef8b
YasserYka <yasseryka@gmail.com>
2020-03-17 16:41:15 +0300

    fixed fas's dcn url and added icons and finished movie.card

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Movie = (props) => {
 
@@ -9,7 +10,14 @@ const Movie = (props) => {
             <div className="card mb-4 shadow-sm">
                 <div className="card-body">
                     <h5>{movie.name}</h5>
-
+                    <p className="card-text">
+                        <strong><i className="fas fa-calendar-alt"> Release Date</i></strong>: {movie.release}
+                        <br/>
+                        <strong><i className="fas fa-star"> Rating</i></strong>: {movie.rating}
+                    </p>
+                    <Link to={`movie/${movie.id}`} className="btn btn-dark btn-block">
+                        <i className="fas fa-chevron-right"></i> Play 
+                    </Link>
                 </div>
             </div>
         </div>
