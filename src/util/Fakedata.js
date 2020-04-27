7f38389d9729b81750d935e5b0c9169fd183cf22
YasserYka <yasseryka@gmail.com>
2020-04-27 18:46:13 +0300

    refactored a lot of things

--- src/util/Fakedata.js
+++ src/util/Fakedata.js
@@ -1,21 +1,21 @@
 const ListOfMovies = [
     {
         id: 243444,
-        name: 'Fake movie #243444',
+        name: 'THE GRAND TOUR',
         release: 2017,
-        rating: 8.0,
+        rating: 8.1,
         poster: '243444.jpg'
     },
     {
         id: 374575,
-        name: 'Fake movie #374575',
+        name: 'EARTH YOU OASIS IN SPACE',
         release: 2019,
         rating: 6.4,
         poster: '374575.jpg'
     },
     {
         id: 457474,
-        name: 'Fake movie #457474',
+        name: 'EUROPA',
         release: 2015,
         rating: 9.1,
         poster: '457474.jpg'
